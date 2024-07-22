9596698c34ead09226e45418d3246d3a29b4b567
Mahendra Chourasiya <mahendra@valuebound.com>
2024-07-22 12:02:39 +0530

    Initial Push

---
+++ src/main/java/com/example/ToDo_App/repo/IToDORepo.java
@@ -0,0 +1,10 @@
+package com.example.ToDo_App.repo;
+
+import com.example.ToDo_App.model.ToDo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IToDORepo extends JpaRepository<ToDo, Long> {
+
+}
